e32eed24851935cde0c0186c981ee92c3d9c732b
Mariano Lopez <mariano.lopez.445@gmail.com>
2020-06-25 20:38:37 -0300

    init project with babel express & nodemon

---
+++ back-end/src/app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', async (req, res) => {
+    res.send('Hello World!')
+});
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
