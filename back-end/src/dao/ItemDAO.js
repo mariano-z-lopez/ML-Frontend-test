8a361bd8c633c9cc72ad701e4f9a8b56fe6ead76
Mariano Lopez <mariano.lopez.445@gmail.com>
2020-06-25 21:02:49 -0300

    add axios http client & Item data access layer implementation

---
+++ back-end/src/dao/ItemDAO.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+const BASE_API_URL= `https://api.mercadolibre.com`
+const SEARCH_API_URL = `${BASE_API_URL}/sites/MLA/search`
+const ITEM_API_URL = `${BASE_API_URL}/items`
+
+export const findAllByQuery = (query) => axios.get(SEARCH_API_URL, {
+    params: {
+        q: query
+    }
+});
+
+export const findItemById = (itemId) => axios.get(`${ITEM_API_URL}/${itemId}`);
+
+export const findItemDescriptionById = (itemId) => axios.get(`${ITEM_API_URL}/${itemId}/description`);
